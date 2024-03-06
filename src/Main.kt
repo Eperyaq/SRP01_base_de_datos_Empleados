1d28c5e0b3618eeee9cf1b08de6432ac966217b8
elia_clase <eperyaq205@g.educaand.es>
2024-03-06 16:02:30 +0100

    corregido

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,37 @@
-class Empleado(val id: Int, var nombre: String, var puesto: String) {
-    fun guardarEmpleado() {
+data class Empleado(val id: Int, var nombre: String, var puesto: String)
+
+class BaseDeDatos {
+    fun guardarEmpleado(empleado: Empleado) {
         // Conexión a la base de datos
         println("Conectando a la base de datos...")
         // Lógica para guardar el empleado
-        println("Empleado $nombre guardado en la base de datos")
+        println("Empleado ${empleado.nombre} guardado en la base de datos")
     }
 
-    fun eliminarEmpleado() {
+    fun eliminarEmpleado(empleado: Empleado) {
         // Conexión a la base de datos
         println("Conectando a la base de datos...")
         // Lógica para eliminar el empleado
-        println("Empleado $nombre eliminado de la base de datos")
+        println("Empleado ${empleado.nombre} eliminado de la base de datos")
     }
+
 }
+
 
 class RealizarInformes(val id: Int, var nombre: String, var puesto: String){
 
-    fun generarInformeEmpleado() {
+    fun generarInformeEmpleado(empleado: Empleado) {
         // Generar un informe del empleado
-        println("Generando informe para el empleado $nombre")
+        println("Generando informe para el empleado ${empleado.nombre}")
     }
 }
 
 fun main() {
     val empleado = Empleado(1, "Juan Pérez", "Desarrollador")
-    empleado.guardarEmpleado()
-    empleado.eliminarEmpleado()
-    val informe = RealizarInformes(1, "Elia","Front-End")
-    informe.generarInformeEmpleado()
+    val db = BaseDeDatos()
+    db.guardarEmpleado(empleado)
+    db.eliminarEmpleado(empleado)
+    val informe = RealizarInformes(1, "Elia", "Front-End")
+    informe.generarInformeEmpleado(empleado)
 }
+
